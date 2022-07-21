44d7f6f2d4163e9dc719d574e34aeba737d32423
P-O Mpolesha <po.mpolesha@gmail.com>
2022-07-21 13:50:02 +0200

    Footer

--- src/components/MainFooter.vue
+++ src/components/MainFooter.vue
@@ -4,7 +4,9 @@
             <p>{{footerTxt}}</p>
             <p>{{footerTxtContact}}</p>
             <div class="main-footer__social-media">
-                <img :key='image.name' v-for="image in footerSocialMedia" v-bind:src="image.path" v-bind:alt="image.name">
+                <a v-bind:href="image.url">
+                    <img :key='image.name' v-for="image in footerSocialMedia" v-bind:src="image.path" v-bind:alt="image.name">
+                </a>
             </div>
         </div>
         <div class="main-footer__bottom">
@@ -18,13 +20,13 @@
         name: 'main-footer',
         data: () => {
             return {
-                footerTxt: 'pour tout vos projets',
-                footerTxtContact: 'contactez moi !',
+                footerTxt: 'pour tous vos projets',
+                footerTxtContact: 'contactez-moi !',
                 footerSocialMedia: [
-                    { name:'linkedIn icone', path:'src/assets/PAGE D\'ACCEUIL/Linkedin-01.png'},
-                    { name:'insta icone', path:'src/assets/PAGE D\'ACCEUIL/Insta-01.png'},
-                    { name:'mail icone', path:'src/assets/PAGE D\'ACCEUIL/Mail-01.png'},
-                    { name:'behance icone', path:'src/assets/PAGE D\'ACCEUIL/Behance-01.png'},
+                    { name:'linkedIn icone', path:'src/assets/PAGE D\'ACCEUIL/Linkedin-01.png', url:'https://www.linkedin.com/in/noémie-pellencin/'},
+                    { name:'insta icone', path:'src/assets/PAGE D\'ACCEUIL/Insta-01.png', url:'https://www.instagram.com/pepe_nome/'},
+                    { name:'mail icone', path:'src/assets/PAGE D\'ACCEUIL/Mail-01.png', url:'mailto:hello@pepenome.fr'},
+                    { name:'behance icone', path:'src/assets/PAGE D\'ACCEUIL/Behance-01.png', url:'https://www.behance.net/pepenome'},
                 ],
                 footerPepenomeLogo: { name:'Pepenome Logo version 1', path:'src/assets/PAGE D\'ACCEUIL/Logo Pepenome1.png'}
             }
