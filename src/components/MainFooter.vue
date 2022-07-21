a2de633d459386710b3968718504fd312682cfa0
BackGrowZ <32708861+BackGrowZ@users.noreply.github.com>
2022-07-21 16:47:08 +0200

    update for build

--- src/components/MainFooter.vue
+++ src/components/MainFooter.vue
@@ -10,7 +10,7 @@
             </div>
         </div>
         <div class="main-footer__bottom">
-            <img v-bind:src="footerPepenomeLogo.path" v-bind:alt="footerPepenomeLogo.name">
+            <img src="../assets/img/Logo-Pepenome1.png" alt="Pepenome Logo">
         </div>
     </footer>
 </template>
@@ -23,12 +23,11 @@
                 footerTxt: 'pour tous vos projets',
                 footerTxtContact: 'contactez-moi !',
                 footerSocialMedia: [
-                    { name:'linkedIn icone', path:'../assets/img/Linkedin-01.png', url:'https://www.linkedin.com/in/noémie-pellencin/'},
-                    { name:'insta icone', path:'../assets/img/Insta-01.png', url:'https://www.instagram.com/pepe_nome/'},
-                    { name:'mail icone', path:'../assets/img/Mail-01.png', url:'mailto:hello@pepenome.fr'},
-                    { name:'behance icone', path:'../assets/img/Behance-01.png', url:'https://www.behance.net/pepenome'},
+                    { name:'linkedIn icone', path:'./src/assets/img/Linkedin-01.png', url:'https://www.linkedin.com/in/noémie-pellencin/'},
+                    { name:'insta icone', path:'./src/assets/img/Insta-01.png', url:'https://www.instagram.com/pepe_nome/'},
+                    { name:'mail icone', path:'./src/assets/img/Mail-01.png', url:'mailto:hello@pepenome.fr'},
+                    { name:'behance icone', path:'./src/assets/img/Behance-01.png', url:'https://www.behance.net/pepenome'},
                 ],
-                footerPepenomeLogo: { name:'Pepenome Logo version 1', path:'../assets/img/Logo-Pepenome1.png'}
             }
         }
     }
