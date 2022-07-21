9848361800d14af25f9bfb30156071abe41641e1
BackGrowZ <32708861+BackGrowZ@users.noreply.github.com>
2022-07-21 16:55:13 +0200

    test for build

--- src/components/MainFooter.vue
+++ src/components/MainFooter.vue
@@ -5,7 +5,7 @@
             <p>{{footerTxtContact}}</p>
             <div class="main-footer__social-media">
                 <a v-for="image in footerSocialMedia" :key='image.name' v-bind:href="image.url" target="_blank">
-                    <img v-bind:src="image.path" v-bind:alt="image.name">
+                    <img v-bind:src="require(image.path)" v-bind:alt="image.name">
                 </a>
             </div>
         </div>
@@ -23,10 +23,10 @@
                 footerTxt: 'pour tous vos projets',
                 footerTxtContact: 'contactez-moi !',
                 footerSocialMedia: [
-                    { name:'linkedIn icone', path:'./assets/img/Linkedin-01.png', url:'https://www.linkedin.com/in/noémie-pellencin/'},
-                    { name:'insta icone', path:'./assets/img/Insta-01.png', url:'https://www.instagram.com/pepe_nome/'},
-                    { name:'mail icone', path:'./assets/img/Mail-01.png', url:'mailto:hello@pepenome.fr'},
-                    { name:'behance icone', path:'./assets/img/Behance-01.png', url:'https://www.behance.net/pepenome'},
+                    { name:'linkedIn icone', path:'../src/assets/img/Linkedin-01.png', url:'https://www.linkedin.com/in/noémie-pellencin/'},
+                    { name:'insta icone', path:'../src/assets/img/Insta-01.png', url:'https://www.instagram.com/pepe_nome/'},
+                    { name:'mail icone', path:'../src/assets/img/Mail-01.png', url:'mailto:hello@pepenome.fr'},
+                    { name:'behance icone', path:'../src/assets/img/Behance-01.png', url:'https://www.behance.net/pepenome'},
                 ],
             }
         }
