7f91340e83a968cf3888939caf746bac88e39c92
BackGrowZ <32708861+BackGrowZ@users.noreply.github.com>
2022-07-21 13:19:02 +0200

    ending router

--- src/views/Project.vue
+++ src/views/Project.vue
@@ -1,13 +1,20 @@
 <template>
 
-Mes projets
+<NavBar/>
+<h1>Mes Projets</h1>
+<MainFooter/>
 
 </template>
 
 <script>
+import NavBar from '../components/NavBar.vue'
+import MainFooter from '../components/MainFooter.vue'
 export default {
-    name: 'contact',
-    components: {}
+    name: 'project',
+    components: {
+        'NavBar': NavBar,
+        'MainFooter': MainFooter
+    }
 }
 
 </script>
