1e25f00536a904f9b1e7f3b3fb159c0cd04fe282
ismaelk93 <ismael.koussoube@yahoo.com>
2022-07-22 17:31:29 +0200

    csscontact

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -7,11 +7,11 @@
     <div class="container">
         <main class="containerContact">
             <div class="contact">
-                <p>Télephone: <a href="#"><strong>06 99 79 00 80</strong></a></p>
+                <p>Télephone: <a href="+33 6 99 79 00 80"><strong>06 99 79 00 80</strong></a></p>
                 <hr class="lineBottom">
-                <p>Mail: <a href="#"><strong>hello@pepenome.fr</strong></a></p>
+                <p>Mail: <a href="mailto:hello@pepenome.fr"><strong>hello@pepenome.fr</strong></a></p>
                 <hr class="lineTop">
-                <p>Adresse:  <strong>94 boulevard des belges <br>&emsp;&thinsp;&ensp;&ensp;&ensp;&ensp; 44300 NANTES</strong></p>
+                <p>Adresse: <a href="https://www.google.com/maps/place/94+Bd+des+Belges,+44300+Nantes/data=!4m2!3m1!1s0x4805ee8db6e26fe5:0xb5dd0fb05317a8a9?sa=X&ved=2ahUKEwj8-8uH6Iz5AhXR8IUKHYrlCWgQ8gF6BAgFEAE"><strong>94 boulevard des belges <br>&emsp;&thinsp;&ensp;&ensp;&ensp;&ensp; 44300 NANTES</strong></a></p>
             </div>
 
             <div class="contacetezMoi">
