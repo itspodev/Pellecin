f8821d3d430a150004b9a3f3a52a24356856986e
P-O Mpolesha <po.mpolesha@gmail.com>
2022-07-25 19:04:13 +0200

    Review Carousel Test

---
+++ src/components/ReviewCarouselSlide.vue
@@ -0,0 +1,21 @@
+<template>
+    <p v-bind:index="index">Index: {{index}}</p>
+    <slot>
+    </slot>
+</template>
+
+<script>
+export default {
+    name: 'ReviewCarouselSlide',
+    data: () => {
+        return {
+            data: () => {
+            },
+            reviews: [
+                { index: 0, textPt1: 'Très heureux du travail de Noémie.', textPt2: 'Nous avons d\'ailleurs décidé de la placer en priorité dans nos futurs projets de graphisme.', author: 'Boxing culture' }
+            ]
+        }
+    },
+    props: [ 'i']
+}
+</script>
