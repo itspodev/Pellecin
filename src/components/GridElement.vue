368eb56261d91bf154d4adcb9c8aaba9fa880f57
Anthony Carreta <32708861+BackGrowZ@users.noreply.github.com>
2022-07-26 19:23:31 +0200

    Update component GridElement

--- src/components/GridElement.vue
+++ src/components/GridElement.vue
@@ -1,10 +1,20 @@
 <template>
-    <div :class="this.project.classContainer" v-if="this.project.tag.includes(this.filter)">
-        <img v-if="this.project.format === 'img'" :class="this.project.classAsset" :src="this.project.img"/>
-        <video v-else-if="this.project.format === 'video'" :class="this.project.classAsset" :src="this.project.img" loop="infinite" autoplay="autoplay" muted=''/>
+    <div v-if="this.tag.includes(this.filter)" :class="`gridItems ${this.project.class}`">
+        <img  
+            v-if="this.project.format === 'img'" 
+            :alt="this.project.name" 
+            :src="this.project.img"
+        />
+        <video 
+            v-else 
+            :src="this.project.img" 
+            loop="infinite" 
+            autoplay="autoplay" 
+            muted=''
+        />
         <div class="text-over">
             <h3>{{this.project.name}}</h3>
-            <p>{{this.project.tag.join(' ')}}</p>
+            <p>{{this.tag.join(', ')}}</p>
         </div>
     </div>
 </template>
@@ -12,6 +22,20 @@
 <script>
 export default{
     name: 'GridElement',
-    props:['project','filter']
+    props:['project','filter'],
+    data() {
+        return {
+            tag: [] 
+        }
+    },
+    methods: {
+        getTag() {
+            this.tag = this.project.tag.filter(tag => tag !== 'home')
+        }
+  },
+  created() {
+    this.getTag()
+  }
+
 };
 </script>
