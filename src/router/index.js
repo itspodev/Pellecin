da89f36b82e904b525297e5184945645e9e3e554
Anthony Carreta <32708861+BackGrowZ@users.noreply.github.com>
2022-07-28 17:39:51 +0200

    add asset for page detail + route + init Detail.vue

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import HomeView from "@/views/HomeAgain.vue";
 import Contact from "@/views/Contact.vue";
 import Project from "@/views/Project.vue";
 import About from "@/views/About.vue";
+import Detail from "@/views/Detail.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -26,6 +27,12 @@ const router = createRouter({
       path: "/about",
       name: "about",
       component: About,
+    },
+    {
+      path: "/project/:name",
+      name: "detail",
+      component: Detail,
+      params: true,
     },
   ],
 });
