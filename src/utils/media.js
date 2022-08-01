b1f04f8e94131866e158a769faf3d274ab917763
ismaelk93 <ismael.koussoube@yahoo.com>
2022-08-01 10:05:30 +0200

    imagedetail

--- src/utils/media.js
+++ src/utils/media.js
@@ -22,6 +22,29 @@ import insta from "@/assets/img/Insta-01.png";
 import mail from "@/assets/img/Mail-01.png";
 import behance from "@/assets/img/Behance-01.png";
 
+import carte from "@/assets/img/LAETITIA/Carte.jpg";
+import insta from "@/assets/img/LEATITIA/Flyer.jpg";
+import flyer from "@/assets/img/LEATITIA/Insta.jpg";
+import logo2 from "@/assets/img/LEATITIA/Logo2.png";
+
+import mockup from "@/assets/img/BLUE/Mockup1.jpg";
+import mockup2 from "@/assets/img/BLUE/Mockup2.jpg";
+import bannière from "@/assets/img/BLUE/bannière.jpg";
+
+import triptique from "@/assets/img/SIXFEET/Triptique_Sixfeet.jpg";
+import paysage from "@/assets/img/SIXFEET/Affiche_Paysage_Sixfeet.jpg";
+import les4sixfeet from "@/assets/img/SIXFEET/Les_4_Sixfeet.jpg";
+import gif from "@/assets/img/SIXFEET/GIF_Bas_de_Page_Sixfeet.gif";
+
+import perso1 from "@/assets/img/N°/Perso1.jpg";
+import perso2 from "@/assets/img/N°/Perso2.jpg";
+import perso3 from "@/assets/img/N°/Perso3.jpg";
+import perso4 from "@/assets/img/N°/Perso4.jpg";
+import perso5 from "@/assets/img/N°/Perso5.jpg";
+import perso6 from "@/assets/img/N°/Perso6.jpg";
+import perso7 from "@/assets/img/N°/Perso7.jpg";
+
+
 export {
   cycliste,
   autoPortrait,
@@ -46,4 +69,23 @@ export {
   insta,
   mail,
   behance,
+
+  carte,
+  flyer,
+  insta,
+  logo2,
+  mockup,
+  insta,
+  bannière,
+  triptique,
+  paysage,
+  les4sixfeet,
+  gif,
+  perso1,
+  perso2,
+  perso3,
+  perso4,
+  perso5,
+  perso6,
+  perso7,
 };
--- src/views/Detail.vue
+++ src/views/Detail.vue
@@ -12,15 +12,156 @@ Pour chaque lien sur les image utiliser le code suivant
 <script>
 import NavBar from '@/components/NavBar.vue'
 import MainFooter from '@/components/MainFooter.vue'
+import GridElement from './GridElement.vue';
 
-
+import {
+    carte,
+    flyer,
+    insta,
+    logo2,
+    mockup,
+    insta,
+    bannière,
+    triptique,
+    paysage,
+    les4sixfeet,
+    gif,
+    perso1,
+    perso2,
+    perso3,
+    perso4,
+    perso5,
+    perso6,
+    perso7,
+} from '@utils/media'
 
 export default {
     name: 'Detail',
     components: {
         'NavBar': NavBar,
         'MainFooter': MainFooter,
-    },
+    props:['home'],
+    data: function() {
+        return {
+            projectArray: [
+                {
+                    name:'Carte',
+                    tag:["motion","web","branding",'édition'],
+                    img: carte,
+                    format: 'img',
+                },
+                {
+                    name: 'Flyer',
+                    tag:["motion","web","branding",'édition'],
+                    img: flyer,
+                    format: 'img',
+                },
+                {
+                    name: 'Insta',
+                    tag:["motion","web","branding",'édition'],
+                    img: insta,
+                    format: 'img',
+                },
+                {
+                    name: 'Logo',
+                    tag:["motion","web","branding",'édition'],
+                    img: logo2,
+                    format: 'img',
+                },
+
+                {
+                    name:'Mockup Blue',
+                    tag:['graphisme','édition'],
+                    img: mockup,
+                    format: 'img',
+                },
+                {
+                    name: 'Mockup2 Blue',
+                    tag:['graphisme','édition'],
+                    img: mockup2,
+                    format: 'img',
+                },
+                {
+                    name: 'Logo Blue',
+                    tag:['graphisme','édition'],
+                    img: bannière,
+                    format: 'img',
+                },
+
+                {
+                    name: 'Triptique sixfeet',
+                    tag:['graphisme','édition'],
+                    img: triptique,
+                    format: 'img',
+                },
+                {
+                    name:'Paysage sixfeet',
+                    tag:['graphisme','édition'],
+                    img: paysage,
+                    format: 'img',
+                },
+                {
+                    name: '4sixfeet',
+                    tag:['graphisme','édition'],
+                    img: les4sixfeet,
+                    format: 'img',
+                },
+                {
+                    name: 'Gif sixfeet',
+                    tag:['graphisme','édition'],
+                    img: gif,
+                    format: 'gif',
+                },
+
+                {
+                    name: 'Perso1 N°',
+                    tag:['graphisme'],
+                    img: perso1,
+                    format: 'img',
+                },
+                {
+                    name: 'Perso2 N°',
+                    tag:['graphisme'],
+                    img: perso2,
+                    format: 'img',
+                },
+                {
+                    name: 'Perso3 N°',
+                    tag:['graphisme'],
+                    img: perso3,
+                    format: 'img',
+                },
+                {
+                    name: 'Perso4 N°',
+                    tag:['graphisme'],
+                    img: perso4,
+                    format: 'img',
+                },
+                {
+                    name: 'Perso5 N°',
+                    tag:['graphisme'],
+                    img: perso5,
+                    format: 'img',
+                },
+                {
+                    name: 'Perso6 N°',
+                    tag:['graphisme'],
+                    img: perso6,
+                    format: 'img',
+                },
+                {
+                    name: 'Perso7 N°',
+                    tag:['graphisme'],
+                    img: perso7,
+                    format: 'img',
+                },
+                
+            ],
+            isTextAnimActive: false
+        }
+    }
+    }
+    
 }
 
 </script>
