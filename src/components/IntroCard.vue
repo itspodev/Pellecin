2cf14b4955ded7a9bc0de3463e5a8d9820e45c56
Anthony Carreta <32708861+BackGrowZ@users.noreply.github.com>
2022-08-05 10:44:34 +0200

    remove .native on event

--- src/components/IntroCard.vue
+++ src/components/IntroCard.vue
@@ -1,52 +1,60 @@
 <template>
-
-<div class="intro-card-box" @mouseenter="mouseOverDiv()" >
-     <div class="intro-pic"> <!--picture frame -->
-        <div class="pola-picture-box" :class="{ 'picAnimation': isPicAnimActive }"><img src="@/assets/img/PHOTO-POLA.png" alt="portrait polaroid de Noémie Pellencin"/></div>
-     </div> 
-    <div class="intro-text">  <!-- text -->
-        <h3>PEPENOME</h3>
-        <div class="intro-name">
-            <h2>Noémie</h2>
-            <h2>PELLENCIN</h2>
-            <hr>
-        </div>
-        <div class="intro-desc">
-            <p>Auto-entrepreneuse depuis février 2021 en tant que <strong>Directrice Artistique et Designer Graphique</strong>.</p>
-            <p>Je <strong>donne vie à vos projets</strong> et suis à l'écoute pour toutes vos demandes: une identité visuelle unique en son genre, une vidéo motion sur mesure,
-            une carte de visite créative, etc.</p>
-            <p><strong>Bienveillance et créativité</strong> sont au coeur des projets que j'entreprends avec mes clients.</p>
-        </div>
-        <h3 class="intro-more"><router-link  @mouseover="mouseOverLink()" @mouseleave="mouseLeaveLink()" class="knwMoreTransition" to="/about" @click.native="scrollToTop"><span :class="{ 'knwMoreAnimation-1': isTextAnimActive }">EN</span> <span :class="{ 'knwMoreAnimation-2': isTextAnimActive }">SAVOIR</span> <span :class="{ 'knwMoreAnimation-3': isTextAnimActive }">PLUS</span></router-link></h3>
-    </div>  
-</div>
-
+  <div class="intro-card-box" @mouseenter="mouseOverDiv()">
+    <div class="intro-pic">
+      <!--picture frame -->
+      <div class="pola-picture-box" :class="{ picAnimation: isPicAnimActive }">
+        <img src="@/assets/img/PHOTO-POLA.png" alt="portrait polaroid de Noémie Pellencin" />
+      </div>
+    </div>
+    <div class="intro-text">
+      <!-- text -->
+      <h3>PEPENOME</h3>
+      <div class="intro-name">
+        <h2>Noémie</h2>
+        <h2>PELLENCIN</h2>
+        <hr />
+      </div>
+      <div class="intro-desc">
+        <p>Auto-entrepreneuse depuis février 2021 en tant que <strong>Directrice Artistique et Designer Graphique</strong>.</p>
+        <p>
+          Je <strong>donne vie à vos projets</strong> et suis à l'écoute pour toutes vos demandes: une identité visuelle unique en son genre, une
+          vidéo motion sur mesure, une carte de visite créative, etc.
+        </p>
+        <p><strong>Bienveillance et créativité</strong> sont au coeur des projets que j'entreprends avec mes clients.</p>
+      </div>
+      <h3 class="intro-more">
+        <router-link @mouseover="mouseOverLink()" @mouseleave="mouseLeaveLink()" class="knwMoreTransition" to="/about" @click="scrollToTop"
+          ><span :class="{ 'knwMoreAnimation-1': isTextAnimActive }">EN</span>
+          <span :class="{ 'knwMoreAnimation-2': isTextAnimActive }">SAVOIR</span>
+          <span :class="{ 'knwMoreAnimation-3': isTextAnimActive }">PLUS</span></router-link
+        >
+      </h3>
+    </div>
+  </div>
 </template>
 
 <script>
-
 export default {
-    name: 'intro-card',
-    data: () => {
-        return {
-            isTextAnimActive: false,
-            isPicAnimActive: false,
-        }
+  name: "intro-card",
+  data: () => {
+    return {
+      isTextAnimActive: false,
+      isPicAnimActive: false,
+    };
+  },
+  methods: {
+    scrollToTop: function () {
+      window.scrollTo(0, 0);
     },
-    methods: {
-        scrollToTop: function() {
-            window.scrollTo(0,0);
-        },
-        mouseOverLink() {
-            this.isTextAnimActive = true;
-        },
-        mouseLeaveLink() {
-            this.isTextAnimActive = false;
-        },
-        mouseOverDiv() {
-            this.isPicAnimActive = true;
-        }
-    },  
-}
-
+    mouseOverLink() {
+      this.isTextAnimActive = true;
+    },
+    mouseLeaveLink() {
+      this.isTextAnimActive = false;
+    },
+    mouseOverDiv() {
+      this.isPicAnimActive = true;
+    },
+  },
+};
 </script>
