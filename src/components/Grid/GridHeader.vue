ca4fe866b01726f1a3a341ddce7d04c6b5e00e90
BackGrowZ <32708861+BackGrowZ@users.noreply.github.com>
2022-08-17 16:09:02 +0200

    update anim

--- src/components/Grid/GridHeader.vue
+++ src/components/Grid/GridHeader.vue
@@ -3,7 +3,11 @@
       <div class="hp-head">
         <p>PEPENOME</p>
         <br />
-        <router-link to="/projets" @click="scrollToTop"><h2 class='border-bottom'>MES PROJETS</h2></router-link>
+        <router-link to="/projets" @click="scrollToTop">
+            <h2 class='border-bottom'>
+                <LetterFlip word="MES PROJETS"/>
+            </h2>
+        </router-link>
         <br />
       </div>
       <GridNavbar :filterArray="this.filterArray" :filter="this.filter"></GridNavbar>
@@ -14,6 +18,7 @@
 <script>
 import GridNavBarMobile from './GridNavBarMobile.vue'
 import GridNavbar from './GridNavbar.vue'
+import LetterFlip from '../letterFlip.vue';
 export default {
     name:'GridHeader',
     props: {
@@ -21,7 +26,7 @@ export default {
         filter:{type:String},
         filterArray:{type:Function}
     },
-    components: { GridNavBarMobile, GridNavbar },
+    components: { GridNavBarMobile, GridNavbar, LetterFlip},
     methods:{
         scrollToTop: function () {
             window.scrollTo(0, 0);
